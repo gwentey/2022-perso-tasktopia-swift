6e68b1103b6f8cdb18b10bae8b4a0b04dd6bc791
Anthony <anthonyoutub@gmail.com>
2022-12-29 23:27:32 +0100

    Initial Commit

---
+++ Tasktopia/TasktopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasktopiaApp.swift
+//  Tasktopia
+//
+//  Created by Anthony RODRIGUES on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TasktopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
